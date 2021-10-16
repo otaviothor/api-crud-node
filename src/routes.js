a93f648d94ee86a808c441ba9c215bec4d6cef73
otavio <otaviosilva2632@gmail.com>
2021-10-16 18:13:26 -0300

    feat: rename route

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 
 const Message = require("./controllers/MessageController");
 
-routes.post("/create", Message.create);
+routes.post("/message", Message.create);
 routes.get("/message", Message.find);
 routes.get("/message/:id", Message.findById);
 routes.put("/message/:id", Message.update);
